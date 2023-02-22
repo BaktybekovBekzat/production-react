29a2fae82eaed1b8bd3f55654ea9e6824e487829
hhsqlp <hhsqlp@gmail.com>
2023-02-22 13:23:33 +0600

    added bundle analyzer

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -2,6 +2,7 @@ import { type BuildOptions } from './types/config'
 import webpack from 'webpack'
 import HtmlWebpackPlugin from 'html-webpack-plugin'
 import MiniCssExtractPlugin from 'mini-css-extract-plugin'
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer'
 
 export function buildPlugins ({
     paths,
@@ -18,6 +19,9 @@ export function buildPlugins ({
         }),
         new webpack.DefinePlugin({
             __IS_DEV__: JSON.stringify(isDev)
+        }),
+        new BundleAnalyzerPlugin({
+            openAnalyzer: false
         })
     ]
 
