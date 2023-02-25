6d9c86c4e29b5d0ddad620dae7d2c3048ce6a244
hhsqlp <hhsqlp@gmail.com>
2023-02-25 19:05:06 +0600

    upgrade sidebar, fixed classNames function, button themes

--- src/shared/ui/Button/Button.test.tsx
+++ src/shared/ui/Button/Button.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
-import { Button, ThemeButton } from './Button'
+import { Button, ButtonTheme } from './Button'
 
 describe('Button component', () => {
     test('Render test', () => {
@@ -9,7 +9,7 @@ describe('Button component', () => {
     })
 
     test('With theme', () => {
-        render(<Button theme={ThemeButton.CLEAR}>TEST</Button>)
+        render(<Button theme={ButtonTheme.CLEAR}>TEST</Button>)
         expect(screen.getByText('TEST')).toHaveClass('clear')
         screen.debug()
     })
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,7 +1,5 @@
 import { BugButton } from 'app/providers/ErrorBoundary'
-import { useTranslation } from 'react-i18next'
 import { classNames } from 'shared/lib/classNames/classNames'
-import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink'
 import cls from './Navbar.module.scss'
 
 interface NavbarProps {
@@ -9,22 +7,10 @@ interface NavbarProps {
 }
 
 export const Navbar = ({ className = '' }: NavbarProps) => {
-    const { t } = useTranslation(['main', 'about'])
-
     return (
         <div className={classNames(cls.Navbar, {}, [className])}>
             <div className={cls.links}>
                 <BugButton />
-                <AppLink
-                    theme={AppLinkTheme.SECONDARY}
-                    className={cls.mainLink}
-                    to={'/'}
-                >
-                    {t('Главная страница')}
-                </AppLink>
-                <AppLink theme={AppLinkTheme.SECONDARY} to={'/about'}>
-                    {t('О сайте', { ns: 'about' })}
-                </AppLink>
             </div>
         </div>
     )
